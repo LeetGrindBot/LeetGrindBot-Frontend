a1108259e830f547e425206a217b1ca95f59271a
Yoni-Deserbaix <yonideserbaix@gmail.com>
2025-01-26 18:14:43 +0100

    feat: add FadeIn animation to layout background images

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import FadeIn from "@/components/animations/FadeIn";
 import type { Metadata } from "next";
 import { Instrument_Sans } from "next/font/google";
 import Image from "next/image";
@@ -23,20 +24,26 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark overflow-x-hidden">
       <body className={`${instrumentSans.className} bg-black`}>
-        <Image
-          src="/images/vector-left.png"
-          alt="logo"
-          height={600}
-          width={600}
-          className="absolute -top-10 left-0 -z-50"
-        />
-        <Image
-          src="/images/vector-right.png"
-          alt="logo"
-          height={1200}
-          width={1200}
-          className="absolute -top-72 -z-50 h-[1000px] object-cover md:-right-36 md:h-[1400px]"
-        />
+        <FadeIn delay={0}>
+          <Image
+            src="/images/vector-left.png"
+            alt="logo"
+            height={600}
+            width={600}
+            className="absolute -top-10 left-0 -z-50"
+          />
+        </FadeIn>
+
+        <FadeIn delay={0.7}>
+          <Image
+            src="/images/vector-right.png"
+            alt="logo"
+            height={1200}
+            width={1200}
+            className="absolute -top-72 -z-50 h-[1000px] object-cover md:-right-36 md:h-[1400px]"
+          />
+        </FadeIn>
+
         {children}
       </body>
     </html>
