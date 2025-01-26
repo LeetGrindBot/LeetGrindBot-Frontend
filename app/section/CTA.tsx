33e9100ae0a880176c60165591a429d07006be6e
Yoni-Deserbaix <yonideserbaix@gmail.com>
2025-01-26 20:23:57 +0100

    feat: add fade animations to CTA section

--- app/section/CTA.tsx
+++ app/section/CTA.tsx
@@ -1,3 +1,4 @@
+import FadeOnScroll from "@/components/animations/FadeOnScroll";
 import { StandaloneShineButton } from "@/components/ui/StandaloneShineButton";
 import { Swords } from "lucide-react";
 import Image from "next/image";
@@ -16,20 +17,24 @@ export default function CTA() {
         />
       </div>
       <div className="relative flex flex-col items-center justify-center gap-4 bg-black/10 py-44 backdrop-blur-sm">
-        <h1 className="mx-auto max-w-3xl text-center text-4xl font-bold tracking-tighter sm:text-6xl">
-          Rejoins des développeurs qui, comme toi <br />
-          <span className="bg-gradient-to-r from-[#2ffeb9] to-[#9f5cf6] bg-clip-text font-bold text-transparent">
-            visent l’excellence.
-          </span>
-        </h1>
-        <Link href="https://discord.gg/y2-empire">
-          <StandaloneShineButton
-            className="mt-12"
-            icon={<Swords className="size-5" />}
-          >
-            Résous ton premier challenge
-          </StandaloneShineButton>
-        </Link>
+        <FadeOnScroll delay={0}>
+          <h1 className="mx-auto max-w-3xl text-center text-4xl font-bold tracking-tighter sm:text-6xl">
+            Rejoins des développeurs qui, comme toi <br />
+            <span className="bg-gradient-to-r from-[#2ffeb9] to-[#9f5cf6] bg-clip-text font-bold text-transparent">
+              visent l’excellence.
+            </span>
+          </h1>
+        </FadeOnScroll>
+        <FadeOnScroll delay={0.2}>
+          <Link href="https://discord.gg/y2-empire">
+            <StandaloneShineButton
+              className="mt-12"
+              icon={<Swords className="size-5" />}
+            >
+              Résous ton premier challenge
+            </StandaloneShineButton>
+          </Link>
+        </FadeOnScroll>
       </div>
     </section>
   );
