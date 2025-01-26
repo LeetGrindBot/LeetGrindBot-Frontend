0cbe41c844a845fd4df03099e42a5649d4f95c84
Yoni-Deserbaix <yonideserbaix@gmail.com>
2025-01-26 18:15:06 +0100

    feat: add fade animations to Hero and Navbar components

--- app/section/Hero.tsx
+++ app/section/Hero.tsx
@@ -3,48 +3,60 @@
 import { Swords } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import FadeUp from "../../components/animations/FadeUp";
 import { StandaloneShineButton } from "../../components/ui/StandaloneShineButton";
 
 export default function Hero() {
   return (
     <section className="pt-14 md:pt-28">
-      <div className="flex items-center justify-center">
-        <div className="inline-flex items-center rounded-full border border-white/40 px-4 py-2 transition-colors duration-300 hover:bg-white/10 max-sm:flex-col sm:gap-3">
-          <span className="bg-[linear-gradient(to_right,#19FB9B,#FB93D0,#FFDD99,#C3F0B2,#2FD8FE)] bg-clip-text text-xs text-transparent sm:text-sm">
-            Si tu rêves de réussir tes entretiens tech...
-          </span>
-          <a
-            href="#features"
-            className="inline-flex items-center gap-1 text-sm"
-          >
-            Découvrir le bot
-          </a>
+      <FadeUp delay={1.5}>
+        <div className="flex items-center justify-center">
+          <div className="inline-flex items-center rounded-full border border-white/40 px-4 py-2 transition-colors duration-300 hover:bg-white/10 max-sm:flex-col sm:gap-3">
+            <span className="bg-[linear-gradient(to_right,#19FB9B,#FB93D0,#FFDD99,#C3F0B2,#2FD8FE)] bg-clip-text text-xs text-transparent sm:text-sm">
+              Si tu rêves de réussir tes entretiens tech...
+            </span>
+            <a
+              href="#features"
+              className="inline-flex items-center gap-1 text-sm"
+            >
+              Découvrir le bot
+            </a>
+          </div>
         </div>
-      </div>
+      </FadeUp>
       <div className="mt-8 flex flex-col items-center space-y-8">
-        <h1 className="text-center text-4xl font-bold tracking-tighter sm:text-6xl">
-          Un défi LeetCode par jour. <br /> Une communauté pour te pousser
-        </h1>
+        <FadeUp delay={1.8}>
+          <h1 className="text-center text-4xl font-bold tracking-tighter sm:text-6xl">
+            Un défi LeetCode par jour. <br /> Une communauté pour te pousser
+          </h1>
+        </FadeUp>
+
         <div className="flex justify-center">
-          <p className="text-md mx-auto max-w-xl text-center text-gray-300 md:text-xl">
-            Libère ton potentiel avec l’énergie d’une communauté soudée qui te
-            guide vers la réussite.
-          </p>
+          <FadeUp delay={2}>
+            <p className="text-md mx-auto max-w-xl text-center text-gray-300 md:text-xl">
+              Libère ton potentiel avec l’énergie d’une communauté soudée qui te
+              guide vers la réussite.
+            </p>
+          </FadeUp>
         </div>
         <div className="flex justify-center gap-2">
-          <Image
-            src="/images/gutsRage.png"
-            alt="Guts Rage"
-            width={100}
-            height={100}
-            className="inline-block size-5"
-          />
-          <span className="text-white/70">
-            +900 développeurs déjà motivés !
-          </span>
+          <FadeUp delay={2.2}>
+            <Image
+              src="/images/gutsRage.png"
+              alt="Guts Rage"
+              width={100}
+              height={100}
+              className="inline-block size-5"
+            />
+          </FadeUp>
+          <FadeUp delay={2.4}>
+            <span className="text-white/70">
+              +900 développeurs déjà motivés !
+            </span>
+          </FadeUp>
         </div>
 
-        <div>
+        <FadeUp delay={2.6}>
           <Link href="https://discord.gg/y2-empire" target="_blank">
             <StandaloneShineButton
               className=""
@@ -53,7 +65,7 @@ export default function Hero() {
               Résous ton premier challenge
             </StandaloneShineButton>
           </Link>
-        </div>
+        </FadeUp>
       </div>
     </section>
   );
--- app/section/Navbar.tsx
+++ app/section/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import FadeDown from "@/components/animations/FadeDown";
 import { StandaloneShineButton } from "@/components/ui/StandaloneShineButton";
 import { useState } from "react";
 import { FaDiscord } from "react-icons/fa";
@@ -26,83 +27,85 @@ export default function Navbar() {
   };
 
   return (
-    <div className="relative" id="header">
-      <div className="mt-4 flex items-center justify-between rounded-full border border-white/10 px-6 py-4 backdrop-blur-sm lg:mx-7">
-        <LogoWithName />
-        <div className="hidden items-center gap-20 lg:flex">
-          <div className="flex flex-row gap-10 font-normal">
-            {headerList.map((item) => (
-              <a
-                href={`#${item.id}`}
-                key={item.id}
-                className="cursor-pointer text-white/80 transition-all hover:text-gray-300"
-              >
-                {item.name}
-              </a>
-            ))}
-          </div>
-          <StandaloneShineButton
-            className="rounded-full px-6 py-3 text-lg"
-            icon={<FaDiscord className="size-5" />}
-          >
-            Accéder à LeetGrindBot
-          </StandaloneShineButton>{" "}
-        </div>
-        <div className="flex items-center lg:hidden">
-          <button
-            onClick={() => {
-              setIsMenuOpen(!isMenuOpen);
-              toggleBurger();
-            }}
-            className="absolute right-2 flex size-20 items-center justify-center"
-            aria-label="Burger menu"
-          >
-            <div className="relative w-full">
-              <div
-                className={`duration- absolute left-1/2 h-[1px] w-[35%] -translate-x-1/2 transform bg-white transition-all duration-500 ${
-                  isBurgerActive ? "top-0 rotate-45" : "-top-1.5"
-                }`}
-              ></div>
-              <div
-                className={`duration- absolute left-1/2 h-[1px] w-[35%] -translate-x-1/2 transform bg-white transition-all duration-500 ${
-                  isBurgerActive ? "top-0 -rotate-45" : "top-1.5"
-                }`}
-              ></div>
+    <FadeDown delay={1.2}>
+      <div className="relative" id="header">
+        <div className="mt-4 flex items-center justify-between rounded-full border border-white/10 px-6 py-4 backdrop-blur-sm lg:mx-7">
+          <LogoWithName />
+          <div className="hidden items-center gap-20 lg:flex">
+            <div className="flex flex-row gap-10 font-normal">
+              {headerList.map((item) => (
+                <a
+                  href={`#${item.id}`}
+                  key={item.id}
+                  className="cursor-pointer text-white/80 transition-all hover:text-gray-300"
+                >
+                  {item.name}
+                </a>
+              ))}
             </div>
-          </button>
-        </div>
-      </div>
-      <div
-        className={`top-18 fixed right-0 z-50 h-screen w-full rounded-tl-2xl text-2xl text-white backdrop-blur-xl backdrop-brightness-50 transition-transform duration-500 ease-in-out lg:hidden ${
-          isMenuOpen ? "translate-x-0" : "translate-x-full"
-        }`}
-      >
-        <div className="flex justify-end p-4"></div>
-        <ul className="mx-8 flex flex-col items-center justify-center gap-8 text-center font-normal">
-          {headerList.map((item) => (
-            <li key={item.id} className="mt-4 w-full">
-              <a
-                href={`#${item.id}`}
-                onClick={() => {
-                  setIsMenuOpen(!isMenuOpen);
-                  toggleBurger();
-                }}
-                className="cursor-pointer transition-all"
-              >
-                {item.name}
-              </a>
-            </li>
-          ))}
-          <li className="w-full">
             <StandaloneShineButton
               className="rounded-full px-6 py-3 text-lg"
               icon={<FaDiscord className="size-5" />}
             >
               Accéder à LeetGrindBot
-            </StandaloneShineButton>
-          </li>
-        </ul>
+            </StandaloneShineButton>{" "}
+          </div>
+          <div className="flex items-center lg:hidden">
+            <button
+              onClick={() => {
+                setIsMenuOpen(!isMenuOpen);
+                toggleBurger();
+              }}
+              className="absolute right-2 flex size-20 items-center justify-center"
+              aria-label="Burger menu"
+            >
+              <div className="relative w-full">
+                <div
+                  className={`duration- absolute left-1/2 h-[1px] w-[35%] -translate-x-1/2 transform bg-white transition-all duration-500 ${
+                    isBurgerActive ? "top-0 rotate-45" : "-top-1.5"
+                  }`}
+                ></div>
+                <div
+                  className={`duration- absolute left-1/2 h-[1px] w-[35%] -translate-x-1/2 transform bg-white transition-all duration-500 ${
+                    isBurgerActive ? "top-0 -rotate-45" : "top-1.5"
+                  }`}
+                ></div>
+              </div>
+            </button>
+          </div>
+        </div>
+        <div
+          className={`top-18 fixed right-0 z-50 h-screen w-full rounded-tl-2xl text-2xl text-white backdrop-blur-xl backdrop-brightness-50 transition-transform duration-500 ease-in-out lg:hidden ${
+            isMenuOpen ? "translate-x-0" : "translate-x-full"
+          }`}
+        >
+          <div className="flex justify-end p-4"></div>
+          <ul className="mx-8 flex flex-col items-center justify-center gap-8 text-center font-normal">
+            {headerList.map((item) => (
+              <li key={item.id} className="mt-4 w-full">
+                <a
+                  href={`#${item.id}`}
+                  onClick={() => {
+                    setIsMenuOpen(!isMenuOpen);
+                    toggleBurger();
+                  }}
+                  className="cursor-pointer transition-all"
+                >
+                  {item.name}
+                </a>
+              </li>
+            ))}
+            <li className="w-full">
+              <StandaloneShineButton
+                className="rounded-full px-6 py-3 text-lg"
+                icon={<FaDiscord className="size-5" />}
+              >
+                Accéder à LeetGrindBot
+              </StandaloneShineButton>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </FadeDown>
   );
 }
